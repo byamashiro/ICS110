779e9ee54c508b6b2eef218be709a2ecd7965809
byamashiro09 <byamashiro09@gmail.com>
2017-06-20 18:29:53 -1000

    draft 2 assignment beta

--- Completed_Exercises/12/assignment_beta.py
+++ Completed_Exercises/12/assignment_beta.py
@@ -3,15 +3,11 @@ import sys
 
 fin = open('AMEX_daily_prices_N.csv')
 header = fin.readline()
-
 data = csv.reader(fin)
 
 list_st_names = []
 for i in data:
 	list_st_names.append(i[1])
-
-#print(list_st_names)
-#sys.exit(0)
 
 counter = {}
 
@@ -22,31 +18,20 @@ for item in list_st_names:
 		counter[item] = 1
 
 
-#print(counter.keys())
-
-
-#for i in counter.keys():
-#	print(i)
-#sys.exit(0)
-
-sum_price = []
-
 for i in counter.keys():
-	print(i)
-	#print(data)
-	for line in data: #code stops running at this line
-		print(line, 'i am doing something')
+	sum_price = []
+	fin.seek(0)
+	#print(i)
+	for line in data:
 		exchange, stock_symbol, date, stock_price_open, stock_price_high, stock_price_low, stock_price_close, stock_volume, stock_price_adj_close = (line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8])
-		print(exchange)
 	
-		if stock_symbol == i:
-			#print(stock_symbol, round((float(stock_price_open) - float(stock_price_close)),2))
+		if stock_symbol == str(i):
 			diff = round((float(stock_price_close) - float(stock_price_open)),2)
 			sum_price.append(diff)
 
-print(sum_price)
-print(round((sum(sum_price)), 2))
-
-	#vol, delta = val(line[5], line[2], line[3])
-	#print(date, stock_price_open, stock_price_close)
+	if round((sum(sum_price)), 2) < -40.0:
+		print(i, ' : ', round((sum(sum_price)), 2))
+	else:
+		continue
+		#print(i, 'does not have a difference less than -40: ', round((sum(sum_price)), 2))
 	 
